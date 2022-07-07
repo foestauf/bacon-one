c3e408d4171e9eed7882fa60c892094702f4abf3
Foestauf <foestauf@gmail.com>
2022-07-07 19:32:39 -0400

    initial commit

---
+++ apps/bacon-one/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'bacon-one',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/bacon-one',
+};
